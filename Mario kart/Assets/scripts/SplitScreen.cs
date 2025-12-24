eb1893cd9421821e25b6203a54cda23b9b269e07
OleksandraRyshniak <oleksandraryshniak@gmail.com>
2025-12-24 18:57:09 +0200

    new item

---
+++ Mario kart/Assets/scripts/SplitScreen.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public Camera cam1;
+    public Camera cam2;
+
+    public bool Horizontal = false;
+    // Update is called once per frame
+    void Update()
+    {
+            ChangeScreen();
+    }
+
+    public void ChangeScreen()
+    {
+            cam1.rect = new Rect(0, 0, 0.5f, 1);
+            cam2.rect = new Rect(0.5f, 0, 0.5f, 1);
+    }
+}
