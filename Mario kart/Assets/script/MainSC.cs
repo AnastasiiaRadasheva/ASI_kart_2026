fdd70c7b0c4fd9ec7ba19bc6ae31e23616da1bc4
Anastasiia Radasheva <eha20082@gmail.com>
2026-01-16 11:05:51 +0200

    meny funk done

--- Mario kart/Assets/script/MainSC.cs
+++ Mario kart/Assets/script/MainSC.cs
@@ -5,7 +5,37 @@ public class MainSC : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void PlayMenu(){
-        SceneManager.LoadScene("cart1");
+        SceneManager.LoadScene("scenes/1player/cart1");
+
+
+    }
+    public void Play1es(){
+        SceneManager.LoadScene("scenes/1player/cart1");
+
+
+    }
+    public void Play1tei(){
+        SceneManager.LoadScene("scenes/1player/cart2");
+
+
+    }
+    public void Play1kolm(){
+        SceneManager.LoadScene("scenes/1player/cart3");
+
+
+    }
+    public void Play2es(){
+        SceneManager.LoadScene("scenes/2players/cart1");
+
+
+    }
+    public void Play2tei(){
+        SceneManager.LoadScene("scenes/2players/cart2");
+
+
+    }
+    public void Play2kolm(){
+        SceneManager.LoadScene("scenes/2players/cart3");
 
 
     }
